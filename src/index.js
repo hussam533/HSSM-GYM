4ddf3f3f583d658d960499451995aebcf0ae66fd
Hussam Jabar <hussamjabar080@gmail.com>
2025-03-28 00:58:26 -0400

    Fix basename for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import BrowserRouter
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Wrap App with BrowserRouter, using PUBLIC_URL so local = "/", GitHub Pages = "/YourSubpath" */}
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
 reportWebVitals();
-
